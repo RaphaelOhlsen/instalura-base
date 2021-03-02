193df334432a67a7b3ca857b77557338b95a30ab
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2021-03-01 21:28:28 -0300

    Finalizando aula04 do modulo02

--- script-test.js
+++ script-test.js
@@ -1,4 +1,9 @@
 const shell = require('shelljs');
 
 console.log('Script-Test');
-shell.exec('git diff --name-only branch-scripts-testes..main');
+const resultado = shell.exec(
+  'git diff --name-only branch-scripts-testes..main',
+  { silent: true }
+);
+
+console.log(resultado.stdout.split('\n'));
