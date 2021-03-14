4f7751023f3d06dac1a2ec35a10eda38510f0187
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2021-03-14 10:36:02 -0300

    Final da aula04Modulo03

--- src/components/wrappers/WebsitePage/index.js
+++ src/components/wrappers/WebsitePage/index.js
@@ -18,6 +18,7 @@ export default function WebSitePageWrapper({
   menuProps,
 }) {
   const [isModalOpen, setModalState] = useState(false);
+
   return (
     <WebsitePageContext.Provider
       value={{
