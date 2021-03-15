342dacccd174e86e765a0233412e177562964b99
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2021-03-15 16:07:57 -0300

    Final da aula05Modulo03

--- src/components/wrappers/WebsitePage/hoc/index.js
+++ src/components/wrappers/WebsitePage/hoc/index.js
@@ -1,11 +1,16 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import WebSitePageWrapper from '..';
 import WebsiteGlobalProvider from '../provider';
 
-export default function websitePageHOC(PageComponent, { pageWrapperProps }) {
+export default function websitePageHOC(
+  PageComponent,
+  { pageWrapperProps } = { pageWrapperProps: {} }
+) {
   return (props) => (
     <WebsiteGlobalProvider>
-      <WebSitePageWrapper {...pageWrapperProps}>
+      <WebSitePageWrapper {...pageWrapperProps} {...props.pageWrapperProps}>
         <PageComponent {...props} />
       </WebSitePageWrapper>
     </WebsiteGlobalProvider>
